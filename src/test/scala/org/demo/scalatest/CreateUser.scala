5fc4cf03135b4917f838efd4380a0839f780484e
LianaLacatus <liana.lacatus@gmail.com>
2015-10-22 21:28:26 +0200

    All cases completed -- except last case -> for you Made everything beautifaaaal!! ERMAGERD STRUCTURED CODE!

--- src/test/scala/org/demo/scalatest/CreateUser.scala
+++ src/test/scala/org/demo/scalatest/CreateUser.scala
@@ -2,6 +2,7 @@ package org.demo.scalatest
 import org.scalatest._
 import org.scalatest.prop.TableDrivenPropertyChecks._
 import domain._
+import org.scalatest.prop.TableDrivenPropertyChecks._
 
 class CreateUser extends FeatureSpec with GivenWhenThen {
   var person: Person = _
@@ -20,14 +21,17 @@ class CreateUser extends FeatureSpec with GivenWhenThen {
     info("I want to be able to register users")
     info("So that I can limit access to the application")
 
+    /**
+     * case 1
+     */
     scenario("The personal details of a user can be registered") {
       firstname = "Bert"
       lastname = "Bertels"
       email = "bert.bertels@gmail.com"
       password = "1PasswordForBert"
-      
+
       given(s"the firstname $firstname, lastname $lastname, email $email and password $password")
-      
+
       when("I choose to create the person with the given data")
       createPerson()
 
@@ -38,14 +42,18 @@ class CreateUser extends FeatureSpec with GivenWhenThen {
       assert(person.getPassword != null)
     }
 
+    /**
+     * case 2
+     * scenario 1
+     */
     scenario("The firstname of a user is not mandatory") {
       firstname = null
       lastname = "Bertels"
       email = "bert.bertels@gmail.com"
       password = "1PasswordForBert"
-      
+
       given(s"the lastname $lastname, email $email and password $password but no firstname")
-      
+
       when("I choose to create the person with the given data")
       createPerson()
 
@@ -56,12 +64,16 @@ class CreateUser extends FeatureSpec with GivenWhenThen {
       assert(person.getPassword != null)
     }
 
+    /**
+     * case 2
+     * scenario 2
+     */
     scenario("the lastname of a user is not mandatory") {
       firstname = "Bert"
       lastname = null
       email = "bert.bertels@gmail.com"
       password = "1PasswordForBert"
-      
+
       given(s"the lastname $lastname, email $email and password $password but no lastname")
 
       when("I choose to create the person with the given data")
@@ -73,9 +85,13 @@ class CreateUser extends FeatureSpec with GivenWhenThen {
       assert(email == person.getUserId)
       assert(person.getPassword != null)
     }
-
+    
+    /**
+     * case 3
+     * scenario 1
+     */
     scenario("The password cannot be stored as plain text") {
-    	password = "PasswordForBert"
+      password = "PasswordForBert"
 
       given(s"the password $password")
 
@@ -88,11 +104,12 @@ class CreateUser extends FeatureSpec with GivenWhenThen {
     }
 
     /**
-     * specification 3
+     * case 3
      * scenario 2
      */
     scenario("different passwords have different hashed values") {
-    	password = "PasswordForBert"
+      password = "PasswordForBert"
+      
       given(s"the password $password and another password OtherPasswordForJan")
       val another_password = "OtherPasswordForJan"
 
@@ -105,15 +122,15 @@ class CreateUser extends FeatureSpec with GivenWhenThen {
     }
 
     /**
-     * specification 3
+     * case 3
      * scenario 3
      */
     scenario("identical passwords have different hashed values") {
-      given("the password PasswordForBert")
-      val password = "PasswordForBert"
+      password = "PasswordForBert"
+      
+      given(s"the password $password")
 
       when("I choose to create a person with this password and I choose to create another person with this password")
-      val person = new Person(email, password, firstname, lastname)
       val another_person = new Person(email, password, firstname, lastname)
 
       then("the stored password of the first person is different from the stored password of the second user")
@@ -121,19 +138,19 @@ class CreateUser extends FeatureSpec with GivenWhenThen {
     }
 
     /**
-     * specification 4
+     * case 4
      * scenario 1
      */
     scenario("the email of a user is mandatory") {
-      given("the firstname Bert, lastname Bertels and password PasswordForBert but no email")
-      val firstname = "Bert"
-      val lastname = "Bertels"
-      val password = "PasswordForBert"
-      val email = null
+      firstname = "Bert"
+      lastname = "Bertels"
+      password = "PasswordForBert"
+      email = null
+      
+      given(s"the firstname $firstname, lastname $lastname and password $password but no email")
 
       when("I choose to create the person with the given data")
-      val person = new Person(email, firstname, lastname, password)
-
+      
       then("an error is given and the person is not created")
       intercept[IllegalArgumentException] {
         createPerson()
@@ -141,19 +158,107 @@ class CreateUser extends FeatureSpec with GivenWhenThen {
     }
 
     /**
-     * specification 4
+     * case 4
      * scenario 2
      */
     scenario("the password of a user is mandatory") {
-      given("the firstname Bert, lastname Bertels and email bert.bertels@gmail.com but no password")
-      val firstname = "Bert"
-      val lastname = "Bertels"
-      val email = "bert.bertels@gmail.com"
-      val password = null
-
+      firstname = "Bert"
+      lastname = "Bertels"
+      email = "bert.bertels@gmail.com"
+      password = null
+      
+      given(s"the firstname $firstname, lastname $lastname and email $email but no password")
+      
       when("I choose to create the person with the given data")
-      createPerson()
+      
+      then("an error is given and the person is not created")
+      intercept[IllegalArgumentException] {
+        createPerson()
+      }
     }
-
+    
+    /**
+     * case 5
+     */
+     scenario("the email of a user should be a valid email address"){
+       given("an email address")
+       val examples = Table (
+           ("email", "motivation"),
+           ("bert@gmail.com", "the local part can have one part"),
+           ("bert.bertels@gmail.com", "the local and domain part can have two parts seperated by a dot"),
+           ("bert.bertels@g.mail.com", "the domain part can have three parts seperated by a dot"),
+           ("1-Be+rt.bertels@gmail.com", "the first local part can contain upper- en lowercase characters, digits, hyphens and plus signs"),
+           ("bert.1ber-Tels@gmail.com", "the second local part can contain upper- en lowercase characters, digits and hyphens"),
+           ("bert.bertels@gMail-1.com", "the first domain part can contain upper- en lowercase characters, digits and hyphens"),
+           ("bert.bertels@gmail.cOm", "the third domain part can contain upper- en lowercase characters")
+           )
+           
+        when("I choose to create a person with this email")
+        
+        then("a person object is created with the given email")
+          forAll(examples){ (mail: String, motivation: String) =>
+            whenever (mail != null){
+              firstname = "Bert"
+              lastname = "Bertels"
+              email = mail
+              password = "1PasswordForBert";
+              
+              createPerson()
+              assert(person != null)
+            }
+       }
+     }
+     
+     /**
+      * case 6
+      * scenario 1
+      */
+     scenario("the local part of an email address can have one part"){
+       email = "bert@gmail.com"
+       
+       given(s"an email address $email")
+       
+       when("I choose to create a person with this email")
+       
+       then("a person object is created with the given email")
+       createPerson()
+       
+       assert(person.getUserId == email)
+     }
+     
+     /**
+      * case 6
+      * scenario 2
+      */
+     scenario("the local and domain part of an email address can have two parts seperated by a dot"){
+       email = "bert.bertels@gmail.com"
+       given(s"an email address $email")
+       
+       when("I choose to create a person with this email")
+       then("a person object is created with the given email")
+       createPerson()
+       assert(person.getUserId == email)
+     }
+     
+     /**
+      * case 6
+      * scenario 3
+      */
+     scenario("the domain part of an email address can have three parts separated by a dot") {
+       email = "bert.bertels@g.mail.com"
+       
+       given(s"an email address $email")
+       
+       when("I choose to create a person with this email")
+       
+       then("a person object is created with the given email")
+       createPerson()
+       assert(person.getUserId == email)
+     }
+     
+     /**
+      * case 7 
+      * TODO
+      */
   }
 }
